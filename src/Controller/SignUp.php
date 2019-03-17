7cc0d6ae80a9cabd6ebe250b71fb42e3b421acc6
BasharAlayan <alayanbashar@gmail.com>
2019-03-17 21:51:02 +0100

    create SignUp Class in controller

--- src/Controller/SignUp.php
+++ src/Controller/SignUp.php
@@ -1,15 +1,40 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: moham
- * Date: 17/03/2019
- * Time: 21:45
- */
 
 namespace App\Controller;
 
 
-class SignUp
-{
+use App\Entity\User;
+use App\Forms\SignUpForm;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
+class SignUp extends AbstractController
+{
+    /**
+     * @Route("/Connection/SignUp")
+     */
+    public function create(Request $request, ObjectManager $objectManager)
+    {
+        $user=new User();
+        $form=$this->createForm(SignUpForm::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $objectManager->persist($user);
+            $objectManager->flush();
+            /*
+                        return $this->redirectToRoute('', [
+                            'slug' => $user->getTitle(),
+                        ]);
+             */
+        }
+
+        return $this->render('Connection/SignUp.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
 }
