2fff71464c52a2bd23ddc40408170bfa6b2dbad9
BasharAlayan <alayanbashar@gmail.com>
2019-04-04 10:26:23 +0200

    improve the objectif page

--- src/Forms/poidForm.php
+++ src/Forms/poidForm.php
@@ -11,20 +11,10 @@ namespace App\Forms;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\RadioType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\DateTime;
-use Symfony\Component\Validator\Constraints\Length;
 
 class poidForm extends AbstractType
 {
@@ -33,9 +23,9 @@ class poidForm extends AbstractType
         $builder
             ->add('weight', IntegerType::class, [
                 'label' => ' ',
-                'attr' => ['placeholder' => 'Entrez votre nouveau poids (en kg)',
-                    'class' => "form-control"]])
-            ->add('valider', SubmitType::class, ['attr' => ['class' => "btn btn-info"]]);
+                'attr' => ['placeholder' => 'Entrez votre nouveau poids (en kg)'
+                ]])
+            ->add('valider', SubmitType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver)
